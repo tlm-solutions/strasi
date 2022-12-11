107d5083d93253fa80ed380566d721c4aa64e5f2
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2022-12-11 22:50:02 +0100

    Fixed the cookie garbage (hopefully) - also moved line number to the center of the background

--- lib/recording_manager.dart
+++ lib/recording_manager.dart
@@ -66,7 +66,13 @@ class _RecordingManagerState extends State<RecordingManager> {
                     },
                     onUpload: () async {
                       final scaffoldMessenger = ScaffoldMessenger.of(context);
-                      await _uploadRecording(widget.database, recordings[index]);
+                      try {
+                        await _uploadRecording(widget.database, recordings[index]);
+                      } on http.ClientException {
+                        scaffoldMessenger.showSnackBar(const SnackBar(
+                            content: Text("We couldn't connect to the KGB server. Is your Internet working?")
+                        ));
+                      }
                       scaffoldMessenger.showSnackBar(const SnackBar(
                           content: Text("Uploaded!")
                       ));
@@ -107,7 +113,9 @@ class _RecordingEntry extends StatelessWidget {
         border: Border.all(color: dvbYellow.shade500),
         borderRadius: const BorderRadius.all(Radius.circular(5)),
       ),
-      child: Row(
+      child: Stack(children: [
+        _OverflowingText("${recording.lineNumber}"),
+        Row(
           children: [
             Flexible(child: Text(
               "#${recording.id}",
@@ -125,33 +133,29 @@ class _RecordingEntry extends StatelessWidget {
             ),
             Flexible(
               flex: 2,
-              child: Stack(
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    _OverflowingText("${recording.lineNumber}"),
-                    Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            IconButton(
-                              onPressed: onExport,
-                              icon: const Icon(Icons.save_alt),
-                            ),
-                            IconButton(
-                              onPressed: onDelete,
-                              icon: const Icon(Icons.delete),
-                            ),
-                            IconButton(
-                              onPressed: onUpload,
-                              icon: const Icon(Icons.upload),
-                            ),
-                          ]
-                        )
-                    )
-                  ],
+                    IconButton(
+                      onPressed: onExport,
+                      icon: const Icon(Icons.save_alt),
+                    ),
+                    IconButton(
+                      onPressed: onDelete,
+                      icon: const Icon(Icons.delete),
+                    ),
+                    IconButton(
+                      onPressed: onUpload,
+                      icon: const Icon(Icons.upload),
+                    ),
+                  ]
+                )
               )
-            ),
+            )
           ]
-      ),
+        )
+      ]),
     );
   }
 }
@@ -163,24 +167,26 @@ class _OverflowingText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRect(
-      child: Opacity(
-        opacity: 0.4,
-        child: Text(
-          text,
-          textAlign: TextAlign.center,
-          maxLines: 1,
-          overflow: TextOverflow.visible,
-          textScaleFactor: 2.1,
-          style: TextStyle(
-            fontSize: 60,
-            height: 0.9,
-            color: dvbYellow.shade900,
-            fontWeight: FontWeight.bold,
-            fontStyle: FontStyle.italic,
+    return Center(
+        child: ClipRect(
+          child: Opacity(
+            opacity: 0.4,
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              maxLines: 1,
+              overflow: TextOverflow.visible,
+              textScaleFactor: 2.1,
+              style: TextStyle(
+                fontSize: 60,
+                height: 0.9,
+                color: dvbYellow.shade900,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
           ),
-        ),
-      ),
+        )
     );
   }
 }
@@ -188,37 +194,37 @@ class _OverflowingText extends StatelessWidget {
 Future<Gpx> _getCoordinatesAsGpx(Future<Database> database, Recording recording) async {
   final db = await database;
 
-  final gpx = Gpx();
-  gpx.metadata = Metadata(
-    name: DateFormat("y-M-d_H-m-s").format(DateTime.now().toUtc()),
-    desc: "Tracked by your friendly stasi comrades.",
-    keywords: "stasi",
-    time: DateTime.now().toUtc(),
-    extensions: {
-      "line": "${recording.lineNumber}",
-      "run": "${recording.runNumber}",
-      "start": recording.start.toIso8601String(),
-      "stop": recording.stop.toIso8601String(),
-    },
-  );
-  gpx.creator = "Stasi for ${io.Platform.operatingSystem} - https://github.com/dump-dvb/stasi";
-  gpx.version = "1.1";
-  gpx.trks = [Trk(
-    trksegs: [Trkseg(
-      trkpts: (await db.query(
-          "cords",
-          columns: ["recording_id", "latitude", "longitude", "speed", "altitude", "time"],
-          where: "recording_id = ?",
-          whereArgs: [recording.id],
-        )).map((cordMap) => Wpt(
-          lat: cordMap["latitude"] as double,
-          lon: cordMap["longitude"] as double,
-          ele: cordMap["altitude"] as double,
-          time: DateTime.parse(cordMap["time"] as String),
-          extensions: {"speed": '${cordMap["speed"] as double}'},
-        )).toList(),
-    )]
-  )];
+  final gpx = Gpx()
+    ..metadata = Metadata(
+      name: DateFormat("y-M-d_H-m-s").format(DateTime.now().toUtc()),
+      desc: "Tracked by your friendly stasi comrades.",
+      keywords: "stasi",
+      time: DateTime.now().toUtc(),
+      extensions: {
+        "line": "${recording.lineNumber}",
+        "run": "${recording.runNumber}",
+        "start": recording.start.toIso8601String(),
+        "stop": recording.stop.toIso8601String(),
+      },
+    )
+    ..creator = "Stasi for ${io.Platform.operatingSystem} - https://github.com/dump-dvb/stasi"
+    ..version = "1.1"
+    ..trks = [Trk(
+      trksegs: [Trkseg(
+        trkpts: (await db.query(
+            "cords",
+            columns: ["recording_id", "latitude", "longitude", "speed", "altitude", "time"],
+            where: "recording_id = ?",
+            whereArgs: [recording.id],
+          )).map((cordMap) => Wpt(
+            lat: cordMap["latitude"] as double,
+            lon: cordMap["longitude"] as double,
+            ele: cordMap["altitude"] as double,
+            time: DateTime.parse(cordMap["time"] as String),
+            extensions: {"speed": '${cordMap["speed"] as double}'},
+          )).toList(),
+      )]
+    )];
 
   return gpx;
 }
@@ -275,36 +281,43 @@ class _Session {
   final String _url;
   io.Cookie? _cookie;
 
-
-  Future<bool> loginUser(_LoginData loginData) async {
+  Future<void> loginUser(_LoginData loginData) async {
     final response = await http.post(
       Uri(scheme: "https", host: _url, path: "/user/login"),
       body: jsonEncode(loginData.toMap()),
       headers: {"Content-Type": "application/json"}
     );
-    if (response.statusCode != 200) return false;
+
+    if (response.statusCode != 200) throw http.ClientException(response.body);
     _updateCookie(response.headers);
-    return true;
   }
 
   Future<_LoginData?> createAccount() async {
     final response = await http.post(Uri(scheme: "https", host: _url, path: "/user/create"));
-    if (response.statusCode != 200) return null;
+    if (response.statusCode != 200) throw http.ClientException(response.body);
     _updateCookie(response.headers);
     final responseDict = jsonDecode(response.body);
     return _LoginData(userId: responseDict["user_id"], password: responseDict["password"]);
   }
 
-  Future<bool> sendGpx(Gpx gpx, Recording recording) async {
-    final gpxRequest = http.MultipartRequest("POST", Uri.parse("https://trekkie.staging.dvb.solutions/travel/submit/gpx"));
-    gpxRequest.files.add(http.MultipartFile.fromString(
-      "yo mama",
-      GpxWriter().asString(gpx, pretty: true),
-      contentType: MediaType("text", "xml"),
-    ));
-    gpxRequest.headers["cookie"] = await _getCookie();
+  Future<void> sendGpx(Gpx gpx, Recording recording) async {
+    final gpxUri = Uri(scheme: "https", host: _url, path: "/travel/submit/gpx");
+
+    final gpxRequest = http.MultipartRequest("POST", gpxUri)
+      ..files.add(http.MultipartFile.fromString(
+        "yo mama",
+        GpxWriter().asString(gpx, pretty: true),
+        contentType: MediaType("text", "xml"),
+      ))
+      ..headers["cookie"] = await _getCookie();
+
     final gpxResponse = await gpxRequest.send();
-    if (gpxResponse.statusCode != 200) return false;
+    if (gpxResponse.statusCode != 200) {
+      final errorCode = gpxResponse.statusCode;
+      final errorMessage = await gpxResponse.stream.bytesToString();
+
+      throw http.ClientException("$errorCode: $errorMessage");
+    }
 
     final timesJson = {
       "gpx_id": jsonDecode(await gpxResponse.stream.bytesToString())["gpx_id"],
@@ -323,36 +336,34 @@ class _Session {
       body: jsonEncode(timesJson),
     );
 
-    return submitResponse.statusCode == 200;
+    if (submitResponse.statusCode != 200) throw http.ClientException(submitResponse.body);
   }
 
   void _updateCookie(Map<String, String> headers) {
     _cookie = io.Cookie.fromSetCookieValue(headers["set-cookie"]!);
   }
 
-  Future<_LoginData> _getLoginData() async {
+  Future<void> _refreshCookie() async {
     final prefs = await SharedPreferences.getInstance();
     final userId = prefs.getString("user_id");
 
     if (userId != null) {
       final password = prefs.getString("password")!;
-      return _LoginData(userId: userId, password: password);
+      await loginUser(_LoginData(userId: userId, password: password));
+      return;
     }
 
     final loginData = (await createAccount())!;
 
     prefs.setString("user_id", loginData.userId);
     prefs.setString("password", loginData.password);
-
-    return loginData;
   }
 
   Future<String> _getCookie() async {
-    if (_cookie == null /* || _cookie!.expires!.isAfter(DateTime.now().toUtc()) */) {
-      final loginData = await _getLoginData();
-      assert (await loginUser(loginData));
+    if (_cookie == null) {
+      await _refreshCookie();
     }
-    return _cookie!.value;
+    return "${_cookie!.name}=${_cookie!.value}";
   }
 }
 
@@ -361,4 +372,3 @@ Future<void> _uploadRecording(Future<Database> database, Recording recording) as
 
   await _Session().sendGpx(gpx, recording);
 }
-
