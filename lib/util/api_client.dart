f26a3041429cd83cb93e1764b5912bd535434c65
TKFRvisionOfficial <52245132+TKFRvisionOfficial@users.noreply.github.com>
2023-04-22 22:20:36 +0200

    Added commit id to api request

--- lib/util/api_client.dart
+++ lib/util/api_client.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'dart:io' as io;
 
 import 'package:flutter/foundation.dart' as foundation;
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
@@ -40,6 +41,7 @@ class ApiClient {
 
   ApiClient._internal(): _url = getURL();
   final String _url;
+  String? _commitId;
   io.Cookie? _cookie;
 
   Future<void> loginUser(LoginData loginData) async {
@@ -64,12 +66,17 @@ class ApiClient {
   }
 
   Future<void> sendGpx(Gpx gpx, Recording recording) async {
+    _commitId ??= await rootBundle.loadString(".git/ORIG_HEAD");
+
     final timesJson = {
       "start": recording.totalStart.toIso8601String(),
       "stop": recording.totalEnd.toIso8601String(),
       "line": recording.lineNumber,
       "run": recording.runNumber,
       "region": recording.regionId,
+      "app_commit": _commitId,
+      "app_name": "stasi",
+      "finished": true,
     };
 
     final submitResponse = await http.post(
